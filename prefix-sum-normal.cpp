c73e604b2ddc78671cd0eb7da633179fe0fe3014
Jingkai Tang <jingkaitang@gmail.com>
2016-06-01 19:40:01 +0800

    mod: prefix sum normal to rand list

--- prefix-sum-normal.cpp
+++ prefix-sum-normal.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
+#include <chrono>
+#include <random>
 #include <algorithm>
-#include <numeric>
-#include <functional>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-  auto x = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int *y = new int[x.size()];
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  minstd_rand0 generator(seed);
+  auto get_rand = [&generator]() {
+    return generator() % 100;
+  };
 
-  // for(int i = 0; i < x.size(); i++) {
-  //   y[i] = accumulate(x.begin(), x.begin()+i+1, 0, plus<int>());
-  // }
-  y[0] = *x.begin();
-  for(int i = 1; i < x.size(); i++) {
-    y[i] = y[i-1] + *(x.begin()+i);
+  int arr_len = 10;
+  int *arr = new int[10];
+  for(int i = 0; i < arr_len; i++) {
+    arr[i] = get_rand();
   }
 
+  auto print_arr = [&arr, &arr_len]() {
+    for_each(arr, arr+arr_len, [](auto el) {
+      cout << el << ' ';
+    });
+  };
+
   cout << "x[] => ";
-  for_each(x.begin(), x.end(), [](auto i) {
-    cout << i << ' ';
-  });
+  print_arr();
   cout << endl;
+
+  for(int i = 1; i < arr_len; i++) {
+    arr[i] += arr[i-1];
+  }
 
   cout << "y[] => ";
-  for_each(y, y+x.size(), [](auto i) {
-    cout << i << ' ';
-  });
+  print_arr();
   cout << endl;
 
-  delete[] y;
   return 0;
 }
